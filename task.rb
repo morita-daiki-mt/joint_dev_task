6c249e58884c3d93d625edce48f89d2d386743e3
morita daiki <morita.daiki.works@gmail.com>
2020-05-17 15:53:45 +0900

    Q10,11,13　修正

--- task.rb
+++ task.rb
@@ -87,21 +87,23 @@ def q10
 
   # 以下に回答を記載
   foods.each do |food|
-    if food.include?("うに")
-      puts "好物です"
-    else
-      puts "まぁまぁ好きです"
-    end
+    message = (food.include? "うに")? "好物です" : "まぁまぁ好きです"
+    p message
   end
+  #   # if food.include?("うに")
+  #   #   puts "好物です"
+  #   # else
+  #   #   puts "まぁまぁ好きです"
+  #   # end
+  # end
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-  sports.flatten!.uniq!
   puts "ユーザーの趣味一覧"
-  sports.each.with_index(1) do |sport, i|
+  sports.flatten!.uniq!.each.with_index(1) do |sport, i|
     puts "No#{i} #{sport}"
   end
 end
@@ -118,7 +120,7 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-  p user_data.merge(update_data)
+  p user_data.merge!(update_data)
 end
 
 def q14
